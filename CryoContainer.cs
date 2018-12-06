3d4996461e755dcbe0dcda4bd4391cff606a7543
User1234 <yaroslav.gurilev@gmail.com>
2018-12-06 16:36:13 +0300

    Small fixes

--- CryoContainer.cs
+++ CryoContainer.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Reflection;
 using CryoDI.Providers;
 using DefaultNamespace;
-using UnityEngine;
 
 namespace CryoDI
 {
--- UnityStarter.cs
+++ UnityStarter.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿#if UNITY_5_3_OR_NEWER
+using UnityEngine;
+#endif
 
 namespace CryoDI
 {
